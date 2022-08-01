ee066b904fb6ad551d308eb8363b096ab8c6f5a6
ILYA <ilya@ILYAs-MacBook-Pro.local>
2022-08-01 22:46:06 +0900

    hero component

---
+++ src/components/Hero.js
@@ -0,0 +1,30 @@
+import AnimatedText from "react-animated-text-content";
+
+const Hero = () => {
+  return (
+    <section className="section1">
+      <ul class="circles">
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+      <div className="container">
+        <div className="hero">
+          <div className="hero__title-container">
+            <h1 className="title__first">Welcome</h1>
+            <h1 className="title__second">To my Portfolio</h1>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
