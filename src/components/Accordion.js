25041af7f884e9450a3769fa35cf4ea2be1d9cc1
den4592 <den4592@naver.com>
2022-08-06 12:28:22 +0900

    tags contents

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,13 +1,16 @@
 import { useState } from "react";
 import clink from "../img/clink.png";
 import secondway_studio from "../img/secondway_studio.png";
+import first_portfolio from "../img/portfolio.png";
+import memorystrike from "../img/memorystrike.png";
+import weddinginvitation from "../img/weddinginvitation.png";
 import { MdDoubleArrow } from "react-icons/md";
 
 const Accordion = () => {
   const [item, setItem] = useState([
     {
       title: "Publishing - CLNK",
-      dev_stack: "HTML CSS JS",
+      dev_stack: "HTML, CSS, JS",
       contents: [clink],
       tags: ["#첫 인턴십", "#인플루언서", "#마케팅 플랫폼", "#퍼블리싱"],
       text: "학교에서 주최하는 산합협동인턴십으로 (주)버즈비터즈 사에서 '클링크' 웹 사이트 개발을 하였다. ",
@@ -15,11 +18,35 @@ const Accordion = () => {
     },
     {
       title: "Publishing - Secondway_Studio",
-      dev_stack: "HTML CSS JS",
+      dev_stack: "HTML, CSS, JS",
       contents: [secondway_studio],
       tags: ["#첫 인턴십", "#라이프스타일", "#멀티 채널", "#네트워크"],
-      text: "(주)버즈비터즈 사의 두번째 사업인 세컨드 웨이 스튜디오 웹 사이트 개발을 하였다.",
+      text: "(주)버즈비터즈 사의 두번째 사업인 '세컨드 웨이 스튜디오' 웹 사이트 개발을 하였다.",
       link: "https://www.secondway.studio",
+    },
+    {
+      title: "First Portfolio",
+      dev_stack: "React, Styled-Components",
+      contents: [first_portfolio],
+      tags: ["#첫 포트폴리오", "#리액트", "styled-components"],
+      text: "리액트와 styled-components를 이용하여 첫 포트폴리오 사이트 개발을 하였다.",
+      link: "https://denill-port.netlify.app",
+    },
+    {
+      title: "Web App - Memory Strike",
+      dev_stack: "React, LocalStorage, AWS",
+      contents: [memorystrike],
+      tags: ["#웹앱", "#리액트", "랜덤카드", "복습", "셀프 테스팅"],
+      text: "공부한 내용을 복습 및 셀프 테스팅을 할 수 있는 웹 앱을 개발하였다.",
+      link: "https://www.memorystrike.com",
+    },
+    {
+      title: "Mobile Wedding Invitation",
+      dev_stack: "MERN Stack, gsap",
+      contents: [weddinginvitation],
+      tags: ["#모바일 청첩장", "#MERN"],
+      text: "MERN stack을 이용한 모바일 청첩장을 개발하였다.",
+      link: "https://byeongju-ilsun.netlify.app",
     },
   ]);
 
